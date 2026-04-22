6b70303e5e35c70245a4f5502250c19f06b8b8cf
Pankaj-70 <grgnld3970@gmail.com>
2026-04-22 09:38:11 +0530

    http requests

--- routing-tutorials/src/app/app.ts
+++ routing-tutorials/src/app/app.ts
@@ -2,18 +2,24 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { ProductService } from './product-service';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, CommonModule],
+  imports: [RouterOutlet, RouterLink, CommonModule, HttpClientModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('routing-tutorials');
+  
   appProducts: string[]=[];
+  httpUsers: any = []; 
   constructor(private prodServiceObj: ProductService) {}
   ngOnInit() {
     this.appProducts = this.prodServiceObj.getProducts();
-  }
+    this.prodServiceObj.getHttpProducts().subscribe((data) => {
+      this.httpUsers = data;
+    });
+  } 
 }
--- routing-tutorials/src/app/product-service.ts
+++ routing-tutorials/src/app/product-service.ts
@@ -1,11 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
+  constructor(private http: HttpClient) {}
   products = ['Sprinkler', 'Socket', 'Obese'];
   getProducts() {
     return this.products;
+  }
+
+  getHttpProducts() {
+    return this.http.get('https://jsonplaceholder.typicode.com/users');
   }
 }

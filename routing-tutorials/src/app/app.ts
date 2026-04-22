f7f9c3222d701ff7054745e4cffa2e70831115e8
Pankaj-70 <grgnld3970@gmail.com>
2026-04-22 08:55:51 +0530

    service

--- routing-tutorials/src/app/app.ts
+++ routing-tutorials/src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { ProductService } from './product-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('routing-tutorials');
+  appProducts: string[]=[];
+  constructor(private prodServiceObj: ProductService) {}
+  ngOnInit() {
+    this.appProducts = this.prodServiceObj.getProducts();
+  }
 }
